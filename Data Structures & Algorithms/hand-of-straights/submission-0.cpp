4ca3cb0b8fcaf83f369fcffcbb755030d9f2bbc8
Neha Ram <neha.ram2786@gmail.com>
2026-08-15 21:34:49 +0530

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size()%groupSize!=0) {
+            return false;
+        }
+
+        map<int,int> count;
+
+        for(int x:hand) {
+            count[x]++;
+        }
+        while(!count.empty()){
+            int start= count.begin()->first;
+
+            for(int i=0;i<groupSize;i++) {
+                int card = start + i;
+
+                if (count[card] == 0)
+                    return false;
+
+                count[card]--;
+
+                if (count[card] == 0)
+                    count.erase(card);
+
+            }
+        }
+
+        return true;
+
+    }
+};
