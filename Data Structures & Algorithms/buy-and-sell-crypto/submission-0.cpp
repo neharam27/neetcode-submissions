1a5de3362ae04785e87b3c6544d7e11d545a9f64
Neha Ram <neha.ram2786@gmail.com>
2026-08-23 21:43:50 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice=prices[0];
+        int maxprofit=0;
+
+        for(int i=0;i<prices.size();i++) {
+
+            int profit= prices[i]-minprice;
+            maxprofit= max(profit,maxprofit);
+            minprice=min(minprice,prices[i]);
+        }
+
+        return maxprofit;
+
+    }
+};
