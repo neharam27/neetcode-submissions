8fa71a8e275eb17ad55d80ab912b481f2de1b9e2
Neha Ram <neha.ram2786@gmail.com>
2026-09-05 21:10:53 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int left=1;
+        int right= *max_element(piles.begin(),piles.end());
+
+        while(left<=right) {
+            int k= left+ (right-left)/2;
+
+            int hours=0;
+
+            for(int pile:piles) {
+                hours+=(pile+k-1)/k; // gives the ceiling
+            }
+            if(hours<=h) {
+                right=k-1;
+            }
+            else{
+                left=k+1;
+            }
+        }
+        return left;
+    }
+};
